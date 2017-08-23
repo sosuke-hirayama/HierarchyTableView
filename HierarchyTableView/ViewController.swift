c427f8dc424480e21ec0062c7fec8450475f9ce1
Sosuke Hirayama <sosuke.hirayama@medley.jp>
2017-08-23 13:52:12 +0900

    Initial Commit

---
+++ HierarchyTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HierarchyTableView
+//
+//  Created by Sosuke Hirayama on 2017/08/23.
+//  Copyright © 2017年 Sosuke Hirayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
